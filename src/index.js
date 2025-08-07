6ec5bdc00d24c23470132d6c8360f65c5088da69
bagoesihsant <bagoesihsant@gmail.com>
2025-08-07 12:36:20 +0700

    Minor bug fix, removing alert.

--- src/index.js
+++ src/index.js
@@ -12,5 +12,3 @@ const calculatorService = new CalculatorService();
 const jokesService = new JokesService();
 
 run(alertService, calculatorService, jokesService);
-
-alert('This message only appear once and you can not escape.');
